26187aa4b290fc6b29c5882097ce64b87495063c
Admin <Admin@DESKTOP-KT6TO1T>
2024-07-31 20:54:19 +0530

    Builder design Solution

--- Exercise/Builder/Exercise1.cs
+++ Exercise/Builder/Exercise1.cs
@@ -1,11 +1,53 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text;
 
 namespace Design_Patterns_in_C_.Exercise.Builder
 {
 
+    class Field
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public override string ToString()
+        {
+            return $"public {Type} {Name};";
+        }
+    }
+    class Class
+    {
+        public string Name { get; set; }
+        public List<Field> Fields { get; set; } = new List<Field>();
+        public Class()
+        {
 
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            sb.Append($"public class {Name}").AppendLine("{");
+            foreach (var field in Fields)
+            {
+                sb.AppendLine($"{field}");
+            }
+            sb.AppendLine("}");
+            return sb.ToString();
+        }
+    }
+    public class CodeBuilder
+    {
+        private Class createClass = new Class();
+        public CodeBuilder(string name)
+        {
+            createClass.Name = name;
+        }
+        public CodeBuilder AddFields(string name, string type)
+        {
+            createClass.Fields.Add(new Field { Name = name, Type = type });
+            return this;
+        }
+        public override string ToString()
+        {
+            return createClass.ToString();
+        }
+    }
 }
--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Design_Patterns_in_C_.Exercise.Builder;
+
 Console.WriteLine("Hello, World!");
+
+// Builder Exe 1
+var test = new CodeBuilder("Mass").AddFields("HERO", "string").AddFields("Director", "string");
+Console.WriteLine(test.ToString());
+
