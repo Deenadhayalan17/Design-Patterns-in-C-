82683c3ff274a3eaf7b0a43369590a0f5d233b0d
Admin <Admin@DESKTOP-KT6TO1T>
2024-08-06 17:38:39 +0530

    Adding PersonFactory

---
+++ Exercise/Factory/Exercise1.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace Design_Patterns_in_C_.Exercise.Factory
+{
+    public class Exercise1
+    {
+        public class Person
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+        }
+        public class PersonFactory
+        {
+            private int Id = 0;
+
+            public Person CreatePerson(string name)
+            {
+                return new Person { Id = Id++, Name = name };
+            }
+            
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,9 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Design_Patterns_in_C_.Exercise.Builder;
+using static Design_Patterns_in_C_.Exercise.Factory.Exercise1;
 
 Console.WriteLine("Hello, World!");
 
 // Builder Exe 1
-var test = new CodeBuilder("Mass").AddFields("HERO", "string").AddFields("Director", "string");
-Console.WriteLine(test.ToString());
+//var test = new CodeBuilder("Mass").AddFields("HERO", "string").AddFields("Director", "string");
+//Console.WriteLine(test.ToString());
 
+// Factory Exe 1
+var factoryTest = new PersonFactory().CreatePerson("mike");
+Console.WriteLine(factoryTest.ToString());
