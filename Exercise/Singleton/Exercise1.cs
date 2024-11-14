1aaac2d371de3e3770466b95bc86011cf9a25ae4
Admin <Admin@DESKTOP-KT6TO1T>
2024-11-14 11:12:03 +0530

    Singleton Exercise

---
+++ Exercise/Singleton/Exercise1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Design_Patterns_in_C_.Exercise.Singleton
+{
+    public class Singleton
+    {
+        public static bool IsSingleton(Func<object> func)
+        {
+            var obj1= func();
+            var obj2= func();
+            return ReferenceEquals(obj1, obj2);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using Design_Patterns_in_C_.Exercise.Builder;
+using Design_Patterns_in_C_.Exercise.Singleton;
 using static Design_Patterns_in_C_.Exercise.Factory.Exercise1;
 
 Console.WriteLine("Hello, World!");
@@ -9,5 +9,13 @@ Console.WriteLine("Hello, World!");
 //Console.WriteLine(test.ToString());
 
 // Factory Exe 1
-var factoryTest = new PersonFactory().CreatePerson("mike");
-Console.WriteLine(factoryTest.ToString());
+//var factoryTest = new PersonFactory().CreatePerson("mike");
+//Console.WriteLine(factoryTest.ToString());
+
+//Singleton Exe 1
+var obj = new object();
+var singltonTest = Singleton.IsSingleton(() => obj);
+Console.WriteLine(singltonTest.ToString());
+var singltonTest2 = Singleton.IsSingleton(() =>  new object());
+Console.WriteLine(singltonTest2.ToString());
+
